afe54b458ee8bf99f1c9f25c1717fc122587fab3
Quy Nguyen Phu <quy.nguyenp@s3corp.com.vn>
2022-06-29 14:50:08 +0700

    init + add spring formatter

---
+++ src/main/java/com/mgmt/bnbbot/BnbBotApplication.java
@@ -0,0 +1,13 @@
+package com.mgmt.bnbbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnbBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BnbBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mgmt/bnbbot/BnbBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgmt.bnbbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BnbBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
